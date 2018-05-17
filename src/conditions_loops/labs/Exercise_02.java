634d5bed4981fda1dcafa7e0fcca755f17cb0c45
maddog0223 <kamishimamadoka1@gmail.com>
2018-05-17 05:00:10 -0400

    finished hw

--- src/conditions_loops/labs/Exercise_02.java
+++ src/conditions_loops/labs/Exercise_02.java
@@ -15,13 +15,16 @@ public class Exercise_02 {
     public static void main(String[] args) {
 
         // create scanner
-
+        Scanner scanner = new Scanner(System.in);
         // prompt user
-
+        System.out.println("Enter the day of the week for Monday:");
         // assign input to variable as int
-
+        int number=scanner.nextInt();
 
         // write completed code here
+        if (number==3) System.out.println("Good job!!");
+        else System.out.println("Incorrect");
+        if (number>7) System.out.println("Remember, there are 7 days in a week!");
 
     }
 }

9804e348e413464c1882214f0dddd7350c214e59
maddog0223 <kamishimamadoka1@gmail.com>
2018-05-31 19:06:39 -0400

    hw

--- src/objects_classes_methods/labs/Exercise_04.java
+++ src/objects_classes_methods/labs/Exercise_04.java
@@ -6,5 +6,35 @@ package objects_classes_methods.labs;
  *      Demonstrate method overloading with at least three overloaded methods.
  *
  */
+class Gather {
+
+    public static void main(String[] args) {
+
+        Overload.over1("hi");
+        Overload.over1(8);
+        Overload.over1(3,5);
 
 
+    }
+
+
+}
+
+
+
+class Overload {
+
+        public static void over1(int a, int b) {
+            int sum = a+b;
+        }
+
+        public static void over1(int a) {
+            int sum = a;
+        }
+
+        public static void over1(String c) {
+            String sum = c + c;
+            System.out.println(sum);
+        }
+
+    }

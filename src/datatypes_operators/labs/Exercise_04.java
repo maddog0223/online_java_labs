2016f1feb68c0cb5b0bfb62bbe9e83fb87972091
maddog0223 <kamishimamadoka1@gmail.com>
2018-06-01 18:09:51 -0400

    hw

--- src/datatypes_operators/labs/Exercise_04.java
+++ src/datatypes_operators/labs/Exercise_04.java
@@ -1,5 +1,7 @@
 package datatypes_operators.labs;
 
+import java.util.Scanner;
+
 /**
  * Data Types and Operators Exercise 4: Arrays calculator
  *
@@ -11,5 +13,28 @@ package datatypes_operators.labs;
  */
 
 public class Exercise_04{
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("In this program you're going enter 10 numbers and I'm going to find the sum");
+
+        int sum = 0;
+
+        for (int i = 0; i<10; i++) {
+
+            System.out.println( "Please enter number" + (i + 1));
+
+            sum += scanner.nextInt();
+
+        }
+
+        double average = sum/10;
+
+
+        System.out.println("The sum is " + sum + " and the average is " + average);
+
+    }
 
 }

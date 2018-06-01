7d2b8e08a471c5e8b3c231b80b89af76a3524341
maddog0223 <kamishimamadoka1@gmail.com>
2018-05-31 23:51:20 -0400

    hw

--- src/objects_classes_methods/labs/Exercise_05.java
+++ src/objects_classes_methods/labs/Exercise_05.java
@@ -8,3 +8,40 @@ package objects_classes_methods.labs;
  *      Use System.out.println to demonstrate when the variables are changed.
  *
  */
+
+
+
+class A {
+
+    String a;
+    static int stat;
+
+    public A(String growth) {
+
+        this.a = growth;
+
+        stat++;
+        System.out.println(growth);
+        System.out.println(stat);
+    }
+
+
+}
+
+class Object {
+
+    public static void main(String[] args) {
+
+            A name1 = new A("Caterpillar");
+
+            A name2 = new A("Cocoon");
+
+            A name3 = new A("Butterfly");
+
+
+
+
+
+    }
+
+}

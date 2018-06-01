7b75964e5ac70ea0c549b3de54b23f340a3e2dea
maddog0223 <kamishimamadoka1@gmail.com>
2018-06-01 15:46:02 -0400

    hw

--- src/datatypes_operators/labs/Exercise_06.java
+++ src/datatypes_operators/labs/Exercise_06.java
@@ -3,7 +3,7 @@ package datatypes_operators.labs;
 /**
  * Data Types and Operators Exercise 6: 2D Array
  *
- *      Creat and populate a 5x5 2D array with multiples of 3, starting with 3. Once populated, print out the results.
+ *      Create and populate a 5x5 2D array with multiples of 3, starting with 3. Once populated, print out the results.
  *
  *      The output should look something like this:
  *
@@ -19,6 +19,34 @@ public class Exercise_06 {
 
     public static void main(String[] args) {
 
-        int[][] twoDArray = new int[5][5];
+        int[][] twoDArray = {
+                {3,6,9,12,15},
+                {18,21,24,27,30},
+                {33,36,38,42,45},
+                {48,51,54,57,60},
+                {63,66,69,72,75}
+        };
+
+
+
+        for (int i = 0; i < 5; i++) {
+
+            for (int x = 0; x < 5; x++) {
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+
+            for (int x = 0; x < 5; x++) {
+
+                System.out.print(twoDArray[i][x] + " ");
+
+                }
+            System.out.println("\n");
+
+        }
+
+
     }
 }
+
--- src/datatypes_operators/labs/Exercise_07.java
+++ src/datatypes_operators/labs/Exercise_07.java
@@ -9,4 +9,29 @@ package datatypes_operators.labs;
  */
 
 public class Exercise_07 {
+
+    public static void main(String[] args) {
+
+        int irreg[][] = {
+
+                {1, 2, 3},
+                {4, 5, 6, 7},
+                {8, 9, 10,}
+        };
+
+
+        for (int[] a : irreg) {
+
+            for (int b : a) {
+
+                System.out.print(b);
+
+            }
+            System.out.println("\n");
+
+        }
+
+    }
 }
+
+

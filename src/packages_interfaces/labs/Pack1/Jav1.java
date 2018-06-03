8f871471c1e5efe6b2cf59619e43386bc7901b1b
maddog0223 <kamishimamadoka1@gmail.com>
2018-06-03 02:07:09 -0400

    package hw

---
+++ src/packages_interfaces/labs/Pack1/Jav1.java
@@ -0,0 +1,23 @@
+package packages_interfaces.labs.Pack1;
+
+public class Jav1 {
+
+
+    public static void main(String args[]){
+
+        Jav1 trans1 = new Jav1();
+
+        trans1.hello();
+
+        }
+
+
+
+
+        protected static void hello(){
+
+        System.out.println("It's protected!!!");
+
+    }
+
+}
---
+++ src/packages_interfaces/labs/Pack2/Jav2.java
@@ -0,0 +1,26 @@
+package packages_interfaces.labs.Pack2;
+
+import packages_interfaces.labs.Pack1.Jav1;
+
+public class Jav2 {
+
+    public static void main(String args[]){
+
+        Jav1 t = new Jav1();
+
+        //cannot transfer "hello method" here from Jav1
+
+
+
+    }
+
+    protected static int haha(int a){
+
+        int i = 3 + a;
+
+        return i;
+
+
+    }
+
+}
